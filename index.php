c4e9ba62c52278761644326f040e61fe018f7d56
xandstrike31 <xandersabino@gmail.com>
2021-09-10 15:52:01 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Accreditation CvSU Indang</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/vendor/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: BizPage - v3.1.1
+  * Template URL: https://bootstrapmade.com/bizpage-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top header-transparent">
+    <div class="container-fluid">
+
+      <div class="row justify-content-center">
+        <div class="col-xl-11 d-flex align-items-center">
+          <a  class="logo mr-auto"> </a>
+          
+    
+
+          <nav class="nav-menu d-none d-lg-block">
+            <ul>
+              <li class="active"><a href="index.php">Home</a></li>
+              <li><a href="#about">Program's</a></li>              
+              <li><a href="bsn.php">BSN</a></li>
+              <li><a href="bsmt.php">BSMT</a></li>                          
+
+            </ul>
+          </nav><!-- .nav-menu -->
+        </div>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Intro Section ======= -->
+  <section id="intro">
+    <div class="intro-container">
+      <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
+
+        <ol class="carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <div class="carousel-item active" style="background-image: url(assets/img/intro-carousel/1.jpg)">
+            <div class="carousel-container">
+              <div class="container">
+                <h2 class="animate__animated animate__fadeInDown"><img src="assets/img/logo.png" alt="" class="img-fluid" height="8%" width="8%"> CvSU Indang</img></h2>
+                <h3><p class="animate__animated animate__fadeInUp"><span style="color:yellow;">AACCUP VIRTUAL ACCREDITATION 2020<br></span>
+					Level III – Phase 2 <br>
+					Bachelor of Science in Nursing </br>
+					Bachelor of Science in Medical Technology / Medical Laboratory Science
+					</p></h3>
+                
+              </div>
+            </div>
+          </div>
+
+   
+        
+
+        </div>
+
+       
+
+      </div>
+	 
+    </div>
+	
+	
+  </section><!-- End Intro Section -->
+
+  <main id="main">
+
+
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about">
+      <div class="container" data-aos="fade-up">
+
+        <header class="section-header">
+          <h3 style="color:#004b18;">PROGRAMS UNDER SURVEY</h3>
+          <p></p>
+        </header>
+
+      <div class="row about-cols">
+
+          <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="about-col">
+              <div class="img">
+                <img src="assets/img/about-plan.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-medkit-outline"></i></div>
+              </div>
+              <h2 class="title"><a href="bsn">Bachelor of Science in Nursing</a></h2>
+              <p>
+                message
+              </p>
+            </div>
+          </div>
+
+          <div class="col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="about-col">
+              <div class="img">
+                <img src="assets/img/about-plan.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-flask-outline"></i></div>
+              </div>
+              <h2 class="title"><a href="bsmt">Bachelor of Science in Medical Technology / Medical Laboratory Science</a></h2>
+              <p>
+               message
+              </p>
+            </div>
+          </div>
+
+       
+
+     </div>
+
+      </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Services Section ======= -->
+    
+
+   
+   
+   
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+         
+         
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+			<strong>Address</strong><br>
+              Cavite State University <br>Indang, Cavite, Philippines <br> <br>           
+             <strong>Contact Number</strong><br><br>
+			 <strong>Fax Number</strong><br><br>
+			
+              <strong>Email</strong> <br>con@cvsu.edu.ph<br>
+            </p>
+
+            <div class="social-links">
+			
+			  <a href="https://www.facebook.com/CaviteStateU" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="https://www.youtube.com/channel/UC6p5srphejusQzaulrxmtow" class="youtube"><i class="fa fa-youtube"></i></a>
+            </div>
+
+          </div>
+		 
+
+          <div class="col-lg-3 col-md-6 footer-newsletter">
+            <h4>Click here to visit our Official Website</h4>
+			<p> <a href="https://cvsu.edu.ph"> <img src="assets/img/website.jpg" alt="" class="img-fluid" > </img></a></p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  
+  
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <!-- Uncomment below i you want to use a preloader -->
+  <!-- <div id="preloader"></div> -->
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
